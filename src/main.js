acfb16f38277709aacf24c21cefed37ec50bc69d
Evan Goss <evan.goss@gmail.com>
2016-10-28 19:29:55 -0700

    Removed console logs and unused code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 var sharedConnections = document.getElementsByClassName('shared-conn-expando');
-console.log(sharedConnections);
-
-// sharedConnections.forEach(function(item) {
-//   item.click();
-//   console.log('click');
-// });
 
 for (var i = 0; i < sharedConnections.length; i++) {
   sharedConnections[i].click();
-  console.log('click');
 }
