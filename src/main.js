6186da452a0ba20c2cb33eb682742bcd47afcf5f
Evan Goss <evan.goss@gmail.com>
2016-10-28 19:24:34 -0700

    Will expand 'shared connections' after page loads

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $('.shared-conn-expando').click();
-});
+var sharedConnections = document.getElementsByClassName('shared-conn-expando');
+console.log(sharedConnections);
+
+// sharedConnections.forEach(function(item) {
+//   item.click();
+//   console.log('click');
+// });
+
+for (var i = 0; i < sharedConnections.length; i++) {
+  sharedConnections[i].click();
+  console.log('click');
+}
